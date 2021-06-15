c9f8684d1eb52d90f34d4d3f2eae6412e7aca038
Quang Nguyen <50561522+qnguyen94@users.noreply.github.com>
2021-06-15 15:24:39 -0700

    Update navbar.js

--- js/fcc_htmlCert/nav/navbar.js
+++ js/fcc_htmlCert/nav/navbar.js
@@ -1,9 +1,9 @@
 let pageList = [
-    ["Home", "/portfolio-page/index.html"],
-    ["Projects Summary", "/portfolio-page/html/fcc_htmlAndCssCert/fcc_html_summary.html"],
-    ["Tribute Page", "/portfolio-page/html/fcc_htmlAndCssCert/tributePage.html"],
-    ["Survey Form","/portfolio-page/html/fcc_htmlAndCssCert/surveyForm.html"],
-    ["Product Landing Page", "/portfolio-page/html/fcc_htmlAndCssCert/productLandingPage.html"]
+    ["Home", "/index.html"],
+    ["Projects Summary", "/fcc_htmlAndCssCert/fcc_html_summary.html"],
+    ["Tribute Page", "/fcc_htmlAndCssCert/tributePage.html"],
+    ["Survey Form","/fcc_htmlAndCssCert/surveyForm.html"],
+    ["Product Landing Page", "/html/fcc_htmlAndCssCert/productLandingPage.html"]
 ]
 
 // var pathname = window.location.pathname;
